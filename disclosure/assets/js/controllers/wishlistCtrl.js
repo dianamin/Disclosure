1694b0944bd4e7c3773722ce75b59da08ab28a94
dianamin <97.diana@gmail.com>
2015-11-20 20:19:59 +0200

    showing wishes

--- disclosure/assets/js/controllers/wishlistCtrl.js
+++ disclosure/assets/js/controllers/wishlistCtrl.js
@@ -1,5 +1,6 @@
 app.controller('wishlistCtrl', function($scope, $http) {
 	$scope.products = [];
+	$scope.wishes = [];
 	$scope.userId;
 
 	console.log($scope.products);
@@ -14,10 +15,27 @@ app.controller('wishlistCtrl', function($scope, $http) {
 		console.log(data);
 	});
 
+	$scope.findProduct = function(id) {
+		for (var i = 0; i < $scope.products.length; i++)
+			if ($scope.products[i].id == id) return $scope.products[i];
+	}
+
+
+	$http({method: 'GET', url: '../wishlist/get'}).success(function(data, status, headers, config) {
+		$scope.wishes = data;
+
+
+		console.log($scope.wishes);
+
+		for (var i = 0; i < $scope.wishes.length; i++)
+			$scope.wishes[i].product = $scope.findProduct($scope.wishes[i].product_id);
+		console.log($scope.wishes);
+	});
+
 	$scope.addWish = function(newWish) {
 		var data = {
 			user_id: $scope.userId,
-			product_id: newWish.product_id,
+			product_id: newWish.productId,
 			price: newWish.price,
 			priority: newWish.priority
 		};
