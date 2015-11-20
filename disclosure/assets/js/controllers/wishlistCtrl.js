9c6063b446b9e560e20caa1b21dfff3f197d5acd
dianamin <97.diana@gmail.com>
2015-11-20 20:00:16 +0200

    adding wishes

--- disclosure/assets/js/controllers/wishlistCtrl.js
+++ disclosure/assets/js/controllers/wishlistCtrl.js
@@ -1,6 +1,31 @@
 app.controller('wishlistCtrl', function($scope, $http) {
 	$scope.products = [];
-	$http({method: 'GET', url: ''}).success(function(data, status, headers, config) {
+	$scope.userId;
+
+	console.log($scope.products);
+	$scope.newWish = {
+		productId: "",
+		price: 0,
+		priority: 25
+	}
+	
+	$http({method: 'GET', url: '/product/get'}).success(function(data, status, headers, config) {
 		$scope.products = data;
+		console.log(data);
 	});
+
+	$scope.addWish = function(newWish) {
+		var data = {
+			user_id: $scope.userId,
+			product_id: newWish.product_id,
+			price: newWish.price,
+			priority: newWish.priority
+		};
+
+		data = JSON.stringify(data);
+		$http.post('../wishlist/create', data).then(function() {
+			alert(data);
+			window.location.reload();
+		});
+	}
 });
