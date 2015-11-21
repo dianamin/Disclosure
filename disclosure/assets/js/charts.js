f0783f364156fa86bf667d8bac498fcf4d214c74
marius92mc <marius_92m@yahoo.com>
2015-11-21 04:40:08 +0200

    Add same dummy chart on Home

--- disclosure/assets/js/charts.js
+++ disclosure/assets/js/charts.js
@@ -2,16 +2,22 @@
 		var lineChartData = {
 			labels : ["January","February","March","April","May","June","July"],
 			datasets : [
-				{
-					label: "My First dataset",
-					fillColor : "rgba(220,220,220,0.2)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					pointHighlightFill : "#fff",
-					pointHighlightStroke : "rgba(220,220,220,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-				},
+					{
+						label: "My First dataset",
+						fillColor : "rgba(220,220,220,0.2)",
+						strokeColor : "rgba(220,220,220,1)",
+						pointColor : "rgba(220,220,220,1)",
+						pointStrokeColor : "#fff",
+						pointHighlightFill : "#fff",
+						pointHighlightStroke : "rgba(220,220,220,1)",
+						data : [randomScalingFactor(),
+						        randomScalingFactor(),
+						        randomScalingFactor(),
+						        randomScalingFactor(),
+						        randomScalingFactor(),
+						        randomScalingFactor(),
+						        randomScalingFactor()]
+					},
 				{
 					label: "My Second dataset",
 					fillColor : "rgba(151,187,205,0.2)",
@@ -20,7 +26,13 @@
 					pointStrokeColor : "#fff",
 					pointHighlightFill : "#fff",
 					pointHighlightStroke : "rgba(151,187,205,1)",
-					data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+					data : [randomScalingFactor(),
+							randomScalingFactor(),
+							randomScalingFactor(),
+							randomScalingFactor(),
+							randomScalingFactor(),
+							randomScalingFactor(),
+							randomScalingFactor()]
 				}
 			]
 
